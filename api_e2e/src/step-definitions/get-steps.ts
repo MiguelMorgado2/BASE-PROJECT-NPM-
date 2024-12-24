87a5f7e431659af1c5e8d92f8105baa5675ee8b3
Miguel <mmorgado1@PMINTL.NET>
2024-12-24 11:03:10 +0000

    section-28-completed

--- api_e2e/src/step-definitions/get-steps.ts
+++ api_e2e/src/step-definitions/get-steps.ts
@@ -1,19 +1,19 @@
 import { Given } from "@cucumber/cucumber"
 import { ScenarioWorld } from "./setup/world";
+import {getResponse} from "../support/rest-helper";
 
 Given(
     /^I retrieve "([^"]*)"$/,
     async function(this: ScenarioWorld, route: string) {
         const {
             api: { request },
-            globalAPIResponseVariables
+            globalAPIResponseVariables,
+            globalConfig
         } = this
 
         console.log(`I retrieve ${route}`)
 
-        const response = await request.get("https://jsonplaceholder.typicode.com/"+route)
-
-        globalAPIResponseVariables.response = response
+        await getResponse(request, route, globalConfig, globalAPIResponseVariables)
 
     }
 )

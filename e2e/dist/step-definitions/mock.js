edd3ae63f8bef245ba1dadd23812e6c6bc832b6b
Miguel Morgado <miguelmorgado2@gmail.com>
2025-01-24 12:48:08 +0000

    dummy-test

--- e2e/dist/step-definitions/mock.js
+++ e2e/dist/step-definitions/mock.js
@@ -13,3 +13,21 @@ var _logger = require("../logger");
   _logger.logger.log(`the ${mockServerKey} endpoint for ${mockConfigKey} is mocked with ${mockPayloadKey}`);
   await (0, _mockBehavior.interceptResponse)(page, mockServerKey, mockConfigKey, mockPayloadKey, globalConfig);
 });
+(0, _cucumber.Given)(/^the cookie "([^"]*)" is set with value "([^"]*)"$/, async function (cookieName, cookieValue) {
+  const {
+    screen: {
+      page
+    }
+  } = this;
+  _logger.logger.log(`Setting cookie ${cookieName} with value ${cookieValue}`);
+  await page.context().addCookies([{
+    name: cookieName,
+    value: cookieValue,
+    domain: 'pt.iqos.com',
+    // You can make this dynamic if needed
+    path: '/',
+    httpOnly: false,
+    secure: true
+  }]);
+  _logger.logger.log(`Cookie ${cookieName} has been set`);
+});

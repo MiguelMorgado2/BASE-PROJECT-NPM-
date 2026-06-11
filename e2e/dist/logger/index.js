07e9cf132181a98ab656a9ea8c49c0820d1137a0
MiguelMorgado2 <miguelmorgado2@gmail.com>
2026-06-11 18:24:36 +0100

    chore: untrack e2e dist folder

--- e2e/dist/logger/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.logger = void 0;
-var _logger = require("./logger");
-var logger = exports.logger = (0, _logger.getLogger)();
